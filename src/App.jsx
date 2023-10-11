4cc589174f64917b1a5380370b50f017611f83a1
steven <Fernandezsteven185@gmail.com>
2023-10-11 18:21:44 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <div className='flex items-center gap-3'>
           <Mode/>
           <img
-            className='w-8 rounded-full' 
+            className='w-8 rounded-full'
             src='https://imgs.search.brave.com/0G6UOCgMYhxiBjowwsnHgM_F1irZk5OSoatLZUrvbSM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9ncmFw/aC5mYWNlYm9vay5j/b20vNTEzMTcxNTMw/L3BpY3R1cmU_dHlw/ZT1sYXJnZQ'
             alt='Not user profile picture'
           />
